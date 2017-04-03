34b626bdcedb18dda3c0af4af3bd0ca4acbe822a
p1509019 <brazier.louis@gmail.com>
2017-04-03 11:58:21 +0200

    Ajout de l'architecture de base du client SMTP.

--- src/utils/Mail.java
+++ src/utils/Mail.java
@@ -1,136 +1,41 @@
 package utils;
 
+import javafx.scene.input.DataFormat;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * @author GregoirePiat
  */
 public class Mail {
 
-    private String fromHeader;
-    private String toHeader;
+    private User fromHeader;
+    private ArrayList<User> toHeader;
     private String subjectHeader;
-    private String dateHeader;
-    private String messageId;
+    private Date dateHeader;
     private String body;
-    private User user;
-
-    private Boolean deleted;
 
     /**
      * Mail constructor
-     * @param headers   ArrayList<String>
-     * @param body      String
-     * @param user      User
+     *
      */
-    public Mail(ArrayList<String> headers, String body, User user){
-
-        this.fromHeader = headers.get(0);
-        this.toHeader = headers.get(1);
-        this.subjectHeader = headers.get(2);
-        this.dateHeader = headers.get(3);
-        this.messageId = headers.get(4);
-        this.body = body;
-        this.user = user;
-        this.deleted = false;
-    }
-
-    public Mail (String string, User user) {
-        String[] splittedMessage = string.split("\\r\\n|\\n|\\r");
-        ArrayList<String> newMail = new ArrayList<String>();
-        for (String split : splittedMessage) {
-            if (!split.equals("")) {
-                newMail.add(split);
-                if (newMail.size() > 5) {
-                    ArrayList<String> headers = new ArrayList<String>();
-                    headers.add(newMail.get(0));
-                    headers.add(newMail.get(1));
-                    headers.add(newMail.get(2));
-                    headers.add(newMail.get(3));
-                    headers.add(newMail.get(4));
-
-                    String body = "";
-                    for (int i = 5; i < newMail.size(); ++i)
-                        body += newMail.get(i) + "\n";
-
-                    /* Create a new message */
-                    this.fromHeader = headers.get(0);
-                    this.toHeader = headers.get(1);
-                    this.subjectHeader = headers.get(2);
-                    this.dateHeader = headers.get(3);
-                    this.messageId = headers.get(4);
-                    this.body = body;
-                    this.user = user;
-                    this.deleted = false;
-                }
-            }
-        }
-    }
-
-
-    public String getFromHeader() {
-        return fromHeader;
-    }
-
-    public void setFromHeader(String fromHeader) {
-        this.fromHeader = fromHeader;
-    }
-
-    public String getToHeader() {
-        return toHeader;
-    }
-
-    public void setToHeader(String toHeader) {
-        this.toHeader = toHeader;
-    }
-
-    public String getSubjectHeader() {
-        return subjectHeader;
-    }
-
-    public void setSubjectHeader(String subjectHeader) {
-        this.subjectHeader = subjectHeader;
-    }
-
-    public String getDateHeader() {
-        return dateHeader;
-    }
-
-    public void setDateHeader(String dateHeader) {
-        this.dateHeader = dateHeader;
-    }
-
-    public String getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId(String messageId) {
-        this.messageId = messageId;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
+    public Mail(User from, ArrayList<User> to, String subject, Date date, String body){
+        this.fromHeader = from;
+        this.toHeader = to;
+        this.subjectHeader = subject;
+        this.dateHeader = date;
         this.body = body;
     }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public int getSize() { return FileManager.formatMailString(this).getBytes().length; }
-
-    public Boolean getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
+    public String toString(){
+        StringBuilder mail = new StringBuilder();
+        mail.append("Date: " + dateHeader.toString()+ "\r\n");
+        mail.append("From: " + fromHeader.getName() + " <" + fromHeader.getMailAddress()+">\r\n");
+        mail.append("Subject: "+ subjectHeader+"\r\n");
+        toHeader.forEach((user)->mail.append("To: "+user.getName()+" <"+user.getMailAddress()+"> ,"));
+        mail.substring(0,mail.length()-1);
+        mail.append("\r\n"+body+"\r\n.\r\n");
+        return mail.toString();
     }
 }
